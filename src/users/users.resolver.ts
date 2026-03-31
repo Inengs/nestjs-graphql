85e16146756d49f2a987e895e6275141bd5a3b3f
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2026-03-31 10:51:03 +0100

    added users.resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args, Int } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
-@Resolver()
-export class UsersResolver {}
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private usersService: UsersService) {}
+
+  @Query(() => [User], { name: 'users' })
+  findAll(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Query(() => User, { name: 'user' })
+  findOne(@Args('id', { type: () => Int }) id: number): User | undefined {
+    return this.usersService.findOne(id);
+  }
+}
