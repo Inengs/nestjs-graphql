886973ca71cf5da88665b1034cfd0d9c116eafa4
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2026-04-08 23:51:12 +0100

    fix: now able to read the generated/prisma file

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,26 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma';
+import { PrismaClient } from '../generated/prisma';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    // const sqlite = new Database('./prisma/dev.db');
+    const adapter = new PrismaBetterSqlite3({ url: './prisma/dev.db' });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    super({ adapter });
+  }
+
   async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 
   async onModuleDestroy() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$disconnect();
   }
 }
